04f86ab776ca56a0d706bfe9dbfcf18ae8504ecf
Josue Lopez <galvez2j@gmail.com>
2019-01-13 23:18:11 -0800

    Initial Commit

---
+++ SwiftCalculator_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalculator_v2
+//
+//  Created by Josue Lopez on 1/13/19.
+//  Copyright © 2019 Josue Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
